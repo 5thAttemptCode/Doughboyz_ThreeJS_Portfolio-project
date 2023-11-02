3c83cad69ce70c4a5d948b38f7055dd77c7e04fe
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2023-11-02 23:05:34 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,34 +1,65 @@
-import * as THREE from 'three';
+import * as THREE from 'three'
+import vertexShader from './shaders/vertex.glsl'
+import fragmentShader from './shaders/fragment.glsl'
 import './style.css'
 
+
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth * 0.7 / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth * 0.7 / window.innerHeight, 0.1, 1000 )
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth * 0.7, window.innerHeight );
-document.body.appendChild( renderer.domElement );
 
-const webGLDiv = document.getElementById('webGL');
-webGLDiv.appendChild( renderer.domElement );
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize( window.innerWidth * 0.7, window.innerHeight )
+document.body.appendChild( renderer.domElement )
 
-const geometry = new THREE.SphereGeometry( 2.8, 64, 64 );
-const material = new THREE.MeshBasicMaterial( { color: 0xf5f5f5 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
 
-camera.position.z = 5;
+const webGLDiv = document.getElementById('webGL')
+webGLDiv.appendChild( renderer.domElement )
+
+
+const geometry = new THREE.SphereGeometry( 2.8, 64, 64 )
+const material = new THREE.RawShaderMaterial({
+    vertexShader: vertexShader,
+    fragmentShader: fragmentShader,
+	uniforms: 
+	{
+		// uFrequency: { value: 3.0 }
+		uFrequency: { value: new THREE.Vector2(2.75, 2.75) },
+		uTime: { value: 0 }
+	}
+})
+
+
+const dough = new THREE.Mesh( geometry, material )
+scene.add( dough )
+camera.position.z = 5
+const clock = new THREE.Clock()
+
 
 function animate() {
-	requestAnimationFrame( animate );
+	const elapsedTime = clock.getElapsedTime()
+	//Update material
+	material.uniforms.uTime.value = elapsedTime
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-
-	renderer.render( scene, camera );
+	requestAnimationFrame( animate )
+	renderer.render( scene, camera )
 }
 
-animate();
+animate()
 
+
+// const menuBtn = document.getElementById("menuBtn")
+// const menu = document.getElementById("menu")
+
+// menuBtn.addEventListener('click', () => {
+// 	menu.classList.toggle('isActive')
+// })
+
+// window.addEventListener('click', (e) => {
+// 	if (e.target !== menuBtn && !menu.contains(e.target)) {
+// 	  menu.classList.remove('isActive');
+// 	}
+//   });
 
 
 window.onblur = function () { document.title = 'OVERPROFED DOUGH!! Come back!!!'; }
